dfddccd8ddf51ed9fb55ef016fc18c952f7e322b
gha3mi <36126354+gha3mi@users.noreply.github.com>
2023-12-29 19:04:27 +0000

    Deploying to gh-pages from @ gha3mi/forbenchmark@13d9f95454d856da7c4b8af5f004f73f533e9e35 🚀

--- src/forbenchmark_coarray.f90
+++ src/forbenchmark_coarray.f90
@@ -1,4 +1,5 @@
 module forbenchmark_coarray
+   !! author: Seyed Ali Ghasemi
 
 #if defined(USE_COARRAY)
 
@@ -12,8 +13,8 @@ module forbenchmark_coarray
    public benchmark
 
    !===============================================================================
-   !> author: Seyed Ali Ghasemi
    type :: mark_co
+   !! author: Seyed Ali Ghasemi
       type(timer) :: time
       real(rk)    :: flops
    end type mark_co
@@ -21,8 +22,8 @@ module forbenchmark_coarray
 
 
    !===============================================================================
-   !> author: Seyed Ali Ghasemi
    type :: mark
+   !! author: Seyed Ali Ghasemi
       character(:), allocatable :: method
       character(:), allocatable :: description
       real(rk)                  :: elapsed_time_min
@@ -37,8 +38,8 @@ module forbenchmark_coarray
 
 
    !===============================================================================
-   !> author: Seyed Ali Ghasemi
    type :: benchmark
+   !! author: Seyed Ali Ghasemi
       type(mark_co), dimension(:), allocatable :: marks_co[:]
       type(mark),    dimension(:), allocatable :: marks
       character(:),                allocatable :: filename
@@ -58,8 +59,8 @@ module forbenchmark_coarray
 contains
 
    !===============================================================================
-   !> author: Seyed Ali Ghasemi
-   elemental impure subroutine init(this, nmarks, title, filename, nloops)
+elemental impure subroutine init(this, nmarks, title, filename, nloops)
+!! author: Seyed Ali Ghasemi
       use, intrinsic :: iso_fortran_env, only: compiler_version, compiler_options
 
       class(benchmark), intent(inout)        :: this
@@ -156,8 +157,8 @@ contains
 
 
    !===============================================================================
-   !> author: Seyed Ali Ghasemi
    impure subroutine start_benchmark(this, imark, method, description, argi, argr)
+   !! author: Seyed Ali Ghasemi
       use face
 
       class(benchmark),       intent(inout)        :: this
@@ -209,8 +210,8 @@ contains
 
 
    !===============================================================================
-   !> author: Seyed Ali Ghasemi
    impure subroutine stop_benchmark(this, imark, flops)
+   !! author: Seyed Ali Ghasemi
       use face
 
       interface
@@ -296,8 +297,8 @@ contains
 
 
    !===============================================================================
-   !> author: Seyed Ali Ghasemi
    impure subroutine write_benchmark(this, imark)
+   !! author: Seyed Ali Ghasemi
       class(benchmark), intent(inout) :: this
       integer,          intent(in)    :: imark
       integer                         :: nunit
@@ -350,8 +351,8 @@ contains
 
 
    !===============================================================================
-   !> author: Seyed Ali Ghasemi
    pure elemental subroutine finalize_mark(this)
+   !! author: Seyed Ali Ghasemi
       class(mark), intent(inout) :: this
 
       if (allocated(this%method)) deallocate(this%method)
@@ -361,8 +362,8 @@ contains
 
 
    !===============================================================================
-   !> author: Seyed Ali Ghasemi
    elemental impure subroutine finalize(this)
+   !! author: Seyed Ali Ghasemi
       class(benchmark), intent(inout) :: this
       integer                         :: nunit
       logical                         :: exist
@@ -400,8 +401,8 @@ contains
 
 
    !===============================================================================
-   !> author: Seyed Ali Ghasemi
    impure function current_date_and_time() result(datetime)
+   !! author: Seyed Ali Ghasemi
       character(21) :: datetime
       character(10) :: date
       character(8)  :: time
