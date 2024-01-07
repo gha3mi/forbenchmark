525505c2c9b086c115ca9ed4dfc46bd68824dac9
Seyed Ali Ghasemi <info@gha3mi.com>
2024-01-07 12:46:52 +0100

    Fix documentation

--- src/forbenchmark.f90
+++ src/forbenchmark.f90
@@ -1,5 +1,5 @@
 module forbenchmark
-   !! summary:: This module is used to switch between the default and coarray versions of the benchmark
+   !! summary: This module is used to switch between the default and coarray versions of the benchmark
    !! author: Seyed Ali Ghasemi
    !! version: {!VERSION!}
    !! license: {!LICENSE!}
