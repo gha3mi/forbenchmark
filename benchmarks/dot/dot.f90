f5a950666e86a515037da7968425641c01bc1c03
Seyed Ali Ghasemi <info@gha3mi.com>
2024-01-24 12:21:47 +0100

    Add a subroutine to prevent compiler optimization

--- benchmarks/dot/dot.f90
+++ benchmarks/dot/dot.f90
@@ -9,7 +9,7 @@ program benchmark_dot
 
    type(benchmark)          :: bench
    real(rk), allocatable    :: u(:), v(:)
-   real(rk), volatile       :: a
+   real(rk)                 :: a
    integer(ik)              :: p
    integer                  :: nl, seed_size, i
    integer, allocatable     :: seed_array(:)
@@ -19,9 +19,9 @@ program benchmark_dot
    allocate(seed_array(seed_size))
    seed_array = 123456789
 
-   call bench%init(7,'Benchmark dot_product','benchmarks/dot/results/dot', 5000)
+   call bench%init(7,'Benchmark dot_product','benchmarks/dot/results/dot', 10000)
 
-   num_elements = [500_ik, 1000_ik, 10000_ik, 100000_ik, 1000000_ik]
+   num_elements = [1000_ik, 10000_ik, 100000_ik, 1000000_ik]
 
    do i = 1, size(num_elements)
       p = num_elements(i)
@@ -41,6 +41,7 @@ program benchmark_dot
       call bench%start_benchmark(1,'dot_product','a = dot_product(u,v)',[p])
       do nl = 1,bench%nloops
          a = dot_product(u,v)
+         call prevent_optimization(a,nl) ! loop-invariant
       end do
       call bench%stop_benchmark(cmp_gflops)
       !===============================================================================
@@ -50,6 +51,7 @@ program benchmark_dot
       call bench%start_benchmark(2,'m1', "a = dot_product(u,v,'m1')",[p])
       do nl = 1,bench%nloops
          a = dot_product(u,v,'m1')
+         call prevent_optimization(a,nl) ! loop-invariant
       end do
       call bench%stop_benchmark(cmp_gflops)
       !===============================================================================
@@ -59,6 +61,7 @@ program benchmark_dot
       call bench%start_benchmark(3,'m2', "a = dot_product(u,v,'m2')",[p])
       do nl = 1,bench%nloops
          a = dot_product(u,v,'m2')
+         call prevent_optimization(a,nl) ! loop-invariant
       end do
       call bench%stop_benchmark(cmp_gflops)
       !===============================================================================
@@ -68,6 +71,7 @@ program benchmark_dot
       call bench%start_benchmark(4,'m3', "a = dot_product(u,v,'m3')",[p])
       do nl = 1,bench%nloops
          a = dot_product(u,v,'m3')
+         call prevent_optimization(a,nl) ! loop-invariant
       end do
       call bench%stop_benchmark(cmp_gflops)
       !===============================================================================
@@ -77,6 +81,7 @@ program benchmark_dot
       call bench%start_benchmark(5,'m4', "a = dot_product(u,v,'m4')",[p])
       do nl = 1,bench%nloops
          a = dot_product(u,v,'m4')
+         call prevent_optimization(a,nl) ! loop-invariant
       end do
       call bench%stop_benchmark(cmp_gflops)
       !===============================================================================
@@ -86,6 +91,7 @@ program benchmark_dot
       call bench%start_benchmark(6,'chunks', "a = fprod(u,v)",[p])
       do nl = 1,bench%nloops
          a = fprod(u,v)
+         call prevent_optimization(a,nl) ! loop-invariant
       end do
       call bench%stop_benchmark(cmp_gflops)
       !===============================================================================
@@ -95,6 +101,7 @@ program benchmark_dot
       call bench%start_benchmark(7,'kahan', "a = fprod_kahan(u,v)",[p])
       do nl = 1,bench%nloops
          a = fprod_kahan(u,v)
+         call prevent_optimization(a,nl) ! loop-invariant
       end do
       call bench%stop_benchmark(cmp_gflops)
       !===============================================================================
@@ -113,6 +120,16 @@ contains
 
       gflops = real(argi(1),kind=rk)*1.0e-9_rk
    end function cmp_gflops
+   !===============================================================================
+
+
+   !===============================================================================
+   ! to prevent compiler from optimizing (loop-invariant)
+   subroutine prevent_optimization(a, nl)
+      real(rk), intent(in) :: a
+      integer, intent(in)  :: nl
+      if (a == 0.0_rk) print*, nl, 'a = 0.0'
+   end subroutine prevent_optimization
    !===============================================================================
 
 end program benchmark_dot
